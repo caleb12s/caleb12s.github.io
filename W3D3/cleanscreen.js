c3bae23dd7e1ed8b7c901851868d00a391d5a39f
caleb12s <60207633+caleb12s@users.noreply.github.com>
2020-05-07 23:35:49 -0500

    Updated

--- W3D3/cleanscreen.js
+++ W3D3/cleanscreen.js
@@ -1,47 +1,84 @@
 $(function() {
-    $(".circles").on("click",disappear);
-    setInterval(increase, 250);
+
+    $("#btn").on('click',function(){
+        
+        let growthRate = parseInt($("#growth_rate").val());
+        let growthAmount = parseInt($("#growth_amount").val());
+        let numberOfCircle = parseInt($("#numberOfCircle").val());
+        let width = parseInt($("#width").val());
+        createCircle(numberOfCircle,width,growthRate,growthAmount);
+    });
+
+    $("#container").on('click','.circles',function(event){
+        disappear(event.target);
+    });
+
+    $("#container").on('mouseover','.circles',function(event){
+        $(event.target).css({
+            'opacity':'1'
+        });
+    })
+    $("#container").on('mouseleave','.circles',function(event){
+        $(event.target).css({
+            'opacity':'0.5'
+        });
+    })
 
    }); 
 
-function disappear() {
-    $(this).remove();
+function disappear(target) {
+    $(target).remove();
    }
 
-function increase(){
-    let growthAmount = $("#growth_amount").val();
-    let growthRate = $("#growth_rate").val();
+function increase(amount){
+    
 
-    $(".circle").css("width", parseInt($("#circle").css("width"))+10+"px");
-    $(".circle").css("height", parseInt($("#circle").css("height"))+10+"px");
+    let prevAmount = $(".circles").css('width');
+    let i = 0;
+    // console.log(parseInt(prevAmount));
+
+    $(".circles").css("width", (parseInt(prevAmount) + parseInt(amount))+"px");
+    $(".circles").css("height", (parseInt(prevAmount)+ parseInt(amount))+"px");
 }
 
 function random(){
-    return Math.floor(Math.random() * 256);
+    return Math.floor(Math.random() * 255);
   }
 
-function createCircle(){
-    let numberOfCircle = $("#numberOfCircle").val();
-    let width = $("#width").val();
-    
+function removeElements(){
+    $(".circles").remove();
+}
+
+
+
+function createCircle(numberOfCircle,width,rate,amount){
+
+    // removeElements();
+
     for(let i = 0; i < numberOfCircle; i++){
         let x = random();
         let y = random();
         let z = random();
         let backgroundColor = `rgb(${x},${y},${z})`;
-        let $circleDiv = $('<div class="circles">')
-        $circleDiv.css({
+        let circleDiv = $('<div class="circles">')
+        circleDiv.css({
             'width': width + 'px',
             'height': width + 'px',
             'background-color': backgroundColor,
-            'left': random() * 2 + 'px',
+            'left': random() *3 + 'px',
+            'border-radius':'50%',
+            'position': 'fixed',
+            'bottom': '50px',
+            'right': '50',
+            'float': 'left',
+            'opacity': '0.5'
         });
-        $("#container").append($circleDiv);
+        $("#container").append(circleDiv);
+
     }
-
-
-
-
+    // setInterval(function(){
+    //     increase(amount);
+    // }, rate);
 }
 
 
