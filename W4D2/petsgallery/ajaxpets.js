2398f70849cc2ff9d72261554aee6c5fed70d2bf
caleb12s <60207633+caleb12s@users.noreply.github.com>
2020-05-12 23:53:36 -0500

    Update ajaxpets.js

--- W4D2/petsgallery/ajaxpets.js
+++ W4D2/petsgallery/ajaxpets.js
@@ -1,32 +1,57 @@
-$(function(){
+// $(function(){
 
-    $("#kitties").on("change",kittiesFunc);
-    $("#puppies").on("change",puppiesFunc)
+//     $("#kitties").on("change",kittiesFunc);
+//     $("#puppies").on("change",puppiesFunc)
 
+// });
+
+// function kittiesFunc(){
+//     // alert("Kitties!");
+//     let animal = "kitty";
+//     $.ajax("ajaxpets.php",
+//     { "type":"GET",
+//       "data":{
+//             "animal":animal
+//         }      
+//     }).done(display);
+// }
+
+// function puppiesFunc(){
+//     // alert("Puppies!");
+//     let animal = "puppy";
+//     $.ajax("ajaxpets.php",
+//     { "type":"GET",
+//       "data":{
+//             "animal":animal
+//         }      
+//     }).done(display);
+// }
+
+// function display(data){
+//     $("#pictures").html(data);
+// }
+
+$(function() {
+    $("#puppies").click(updatePictures);
+    $("#kitties").click(updatePictures);
 });
 
-function kittiesFunc(){
-    // alert("Kitties!");
-    let animal = "kitty";
-    $.ajax("ajaxpets.php",
-    { "type":"GET",
-      "data":{
-            "animal":animal
-        }      
-    }).done(display);
+function updatePictures() {
+    var animal = "";
+    if ($("#puppies").attr("checked") == "checked") {
+        animal = "puppy";
+    } else {
+        animal = "kitty";
+    }
+    $.ajax("ajaxpets.php", {
+		"type": "get",
+		"data": {
+        	"animal": animal
+		}
+    }).done(displayPictures);
 }
 
-function puppiesFunc(){
-    // alert("Puppies!");
-    let animal = "puppy";
-    $.ajax("ajaxpets.php",
-    { "type":"GET",
-      "data":{
-            "animal":animal
-        }      
-    }).done(display);
-}
-
-function display(data){
+function displayPictures(data) {
     $("#pictures").html(data);
 }
+
